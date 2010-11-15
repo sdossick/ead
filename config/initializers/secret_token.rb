e1ff6deb710c8947756c50656cae29f58488b091
Steve Dossick <sdossick@martian.com>
2010-11-15 14:24:44 -0800

    ead

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ead::Application.config.secret_token = '8d71f17167140e0919dbe808ddbf9f797625995a15274b89e7e4901a351cf83a9df0d53ebdeae01d558fd413fb1c57af1b2033ecf7d8cbed3d6b6988c8358b05'
